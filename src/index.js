c29ee540209421e7d01a794437c8ddbe7b20700c
Heidi Rodriguez <hrod2645@gmail.com>
2017-03-08 10:09:18 -0700

    configure index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import ReactDOM from 'react-dom';
 import routes from './routes';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+injectTapEventPlugin();
 
-render(
-  <Router history={browserHistory} routes={routes} />,
-  document.getElementById('app')
+ReactDOM.render(
+    <MuiThemeProvider>
+      <Router />
+    </MuiThemeProvider>,
+  document.getElementById('root')
 );
